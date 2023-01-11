3ad812c32028b8512b6d7cd96f093f5a3e0e7a12
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-01-11 11:52:32 +0530

    :fire: created all the schema rerquired for the shareKaro

--- shareKaro_backend/schemas/index.js
+++ shareKaro_backend/schemas/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import user from "./user";
+import postedBy from "./postedBy";
+import comment from "./comment";
+import pin from "./pin";
+import save from "./save";
+
+
+export const schemaTypes = [user, comment, pin, save, postedBy];
