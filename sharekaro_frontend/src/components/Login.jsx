f4aaebca5d2515635762e9866c52254df4af08c4
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-01-18 12:36:25 +0530

    :fire: completed the google login

--- sharekaro_frontend/src/components/Login.jsx
+++ sharekaro_frontend/src/components/Login.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
+import {GoogleLoginButton} from "react-social-login-buttons";
+import {LoginSocialGoogle} from "reactjs-social-login";
 import { useNavigate } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
 const Login = () => {
-  const responseGoogle = (response) => {
-    console.log(response);
-  }
+  // const responseGoogle = (response) => {
+  //   console.log(response);
+  // }
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -26,22 +26,20 @@ const Login = () => {
             <img src={logo} width="130px" alt="logo" />
           </div>
           <div className="shadow-2xl">
-            <GoogleLogin
-              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sign in with google
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
-            />
+            <LoginSocialGoogle
+              client_id={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              scope="openid profile email"
+              discoveryDocs="claims_supported"
+              access_type="offline"
+              onResolve={({ provider, data }) => {
+                console.log(provider, data);
+              }}
+              onReject={(err) => {
+                console.log(err);
+              }}
+            >
+               <GoogleLoginButton />
+            </LoginSocialGoogle>
           </div>
         </div>
       </div>
